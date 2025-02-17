ccc14c79f24b2344c7ec33ba538f70115d7c7cee
yashiro <202412ap01_yashiro@x-ide.sakura.ne.jp>
2025-02-17 12:33:39 +0900

    #35446　仕入登録　物件名を入力して登録しても「正しい内見日付を入力してください」のメッセージが表示され登録されない

--- SRC/js/common.js
+++ SRC/js/common.js
@@ -7,6 +7,9 @@
 function fnYMDCheck(msg, obj) {
 	// 未入力時はチェックしない
 	oYMD = obj.value;
+	if (!oYMD) {
+		return true;
+	}
 
 	var tmp = oYMD.split('/');
 	var ymd = new Date(tmp[0], parseInt(tmp[1], 10) - 1, parseInt(tmp[2], 10));
