208f15185cadbe632d8afac0e487b001ca48816f
yashiro <202412ap01_yashiro@x-ide.sakura.ne.jp>
2025-03-12 12:49:06 +0900

    #37222  専有面積が小数点以下３桁で登録できる

--- SRC/js/sell.js
+++ SRC/js/sell.js
@@ -71,7 +71,7 @@ function fnSellEditCheck() {
 		alert('専有面積を入力してください');
 		return;
 	}
-	if (tmp.length > 6 || tmp.match(/[^0-9\.]+/)) {
+	if (tmp.length > 0 && !tmp.match(/^([1-9][0-9]{0,2}|0)(\.[0-9][0-9]|\.[0-9])?$/)) {
 		alert('専有面積は3桁以内（小数点以下2桁以内）の半角数字で入力してください');
 		return;
 	}
